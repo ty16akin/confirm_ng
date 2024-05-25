078bde857c748d7b7bb387c6429bcb03284b6e92
ty_akins <ty16akins@gmail.com>
2024-05-25 17:10:46 -0400

    Restarting

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,1 @@
 package main
-
-import (
-	"context"
-	"fmt"
-
-	"github.com/ty16akin/ConfirmNG/application"
-)
-
-func main() {
-	app := application.New()
-
-	err := app.Start(context.TODO())
-	if err != nil {
-		fmt.Println("failed to start app:", err)
-	}
-}
